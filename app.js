36879d89b7a8a3b0c622f3f45440f28c171c6f3f
Ian Sibner <sibnerian@gmail.com>
2015-02-05 15:26:31 -0500

    Middleware example

--- app.js
+++ app.js
@@ -4,6 +4,35 @@ app.set('port', process.env.PORT || 3000);
 app.get('/', function (req, res) {
   return res.send('hello world!');
 });
+
+/////////////////////////////////
+//          Middleware         //
+/////////////////////////////////
+
+// This runs on every route (endpoint)
+app.use(function (req, res, next) {
+    console.log('I am a middleware!');
+    next();
+});
+
+// This runs ONLY on '/middleware/test'
+app.use('/middleware/test', function (req, res, next) {
+    console.log('I only run on /middleware/test');
+    next();
+});
+
+// This demonstrates the fact that middlewares can change
+// the req/res objects as needed to handle requests
+var setFoo = function (req, res, next) {
+    req.foo = 'some value';
+    next();
+};
+var logFoo = function (req, res, next) {
+    console.log(req.foo);
+    next();
+};
+app.use(setFoo, logFoo);
+
 // Start listening for requests
 app.listen(app.get('port'), function () {
   console.log('Listening on port ' + app.get('port'));
--- readme.md
+++ readme.md
@@ -8,6 +8,19 @@ $ node app.js
 
 ### Check it out
 
-Navigate to http://localhost:3000. You should see "hello world" in your browser. Yes, it's that easy!
+Navigate to http://localhost:3000. You should see "hello world" in your browser.
 
-Don't forget to check out the other branches for examples of middleware, routers, etc!
+Now that we've added some middlewares, you should see some things getting logged to the server console (in your terminal). Navigating to ANY route should cause it to print
+
+```bash
+I am a middleware!
+some value
+```
+
+If you navigate to http://localhost:3000/middleware/test, it won't look great in the browser, but you should see an extra log from the route-specific middleware we added:
+
+```bash
+I am a middleware!
+I only run on /middleware/test
+some value
+```
